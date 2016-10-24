b1d23fa01c7dbd2aa199ec22c9fc04759e5d0e60
Ryan McCleary <19venom84@gmail.com>
2016-10-24 11:24:50 -0400

    note

--- r-2ndlecture-19venom84.R
+++ r-2ndlecture-19venom84.R
@@ -1,6 +1,14 @@
+# test
 # 1. Write a loop that prints out the numbers from 20 to 10
-for(i in 20:10) {
-  print(i)
+for(a in 20:10) {
+  print(a)
 }
 
-#2.  Write a loop that prints out only the numbers from 20 to 10 that are even
+# 2.  Write a loop that prints out only the numbers from 20 to 10 that are even
+for(b in 20:10) {
+  if(b %% 2 == 0)
+    print(b)
+}
+
+# 3.  Write a function that calculates whether a number is a prime number 
+#     (hint: what does 2 %% 3 give you?)
